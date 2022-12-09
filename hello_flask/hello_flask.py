d18cb3641e46f41eacf91e253e0ee1557b666ef9
Ivan <prigulnov88@gmail.com>
2022-12-09 12:10:24 +0500

    hello_flask was changed, viewlog.html added

--- hello_flask/hello_flask.py
+++ hello_flask/hello_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request  # redirect
+from flask import Flask, render_template, request, escape  # redirect
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -7,8 +7,8 @@ app = Flask(__name__)
 # def hello() -> '302':
 #     return redirect('/entry')
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+    with open('../vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep=' | ')
 
 
 @app.route('/search4', methods=['POST'])
@@ -33,6 +33,21 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the Web!')
+
+
+@app.route('/viewlog')
+def viewlog() -> 'html':
+    contents = []
+    with open('../vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 
 if __name__ == '__main__':
