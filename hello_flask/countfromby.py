1a835401bad991b93c869148862f4dd0e6d3afab
Ivan <prigulnov88@gmail.com>
2023-01-15 00:35:10 +0500

    file for studying classes

---
+++ hello_flask/countfromby.py
@@ -0,0 +1,21 @@
+class CountFromBy:
+    def __init__(self, val=0, incr=1):
+        self.val = val
+        self.incr = incr
+
+    def increase(self) -> None:
+        self.val += self.incr
+
+    def show_atr(self):
+        print(f'val={self.val} incr={self.incr}')
+
+
+if __name__ == '__main__':
+    b = CountFromBy(50, 10)
+    a = CountFromBy()
+    b.increase()
+    for i in range(5):
+        a.increase()
+    a.increase()
+    b.show_atr()
+    a.show_atr()
