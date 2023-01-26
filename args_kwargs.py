51160f436238651220a2a0e1e077170a244d3313
Ivan <prigulnov88@gmail.com>
2023-01-26 17:21:36 +0500

    args, kwargs study

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,6 +1,6 @@
 def myfunc(*args):
     for _ in args:
-        print(_, sep='->', end=' ')
+        print(_, end=' ')
     print()
 
 
@@ -10,6 +10,15 @@ def myfunc2(**kwargs):
     print()
 
 
+def myfunc3(*args, **kwargs):
+    for _ in args:
+        print(_, end=' ')
+    for k, v in kwargs.items():
+        print(k, v, sep='->', end=' ')
+    if kwargs:
+        print()
+
+
 if __name__ == '__main__':
     val_1 = [1, 2, 3]
     val_2 = [4, 5, 6]
@@ -17,5 +26,9 @@ if __name__ == '__main__':
 
     myfunc(1, 2, 'три')
     myfunc(*val_1, val_2)
+    print()
     myfunc2(one=1, two=2)
     myfunc2(**num)
+    print()
+    myfunc3(1, 2, 'три', a=10, b=20)
+    myfunc3(*val_1, val_2, **num, abc=123)
