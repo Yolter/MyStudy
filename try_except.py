058ad134e412978fa4ba086d554f8b38dce1221d
Ivan <prigulnov88@gmail.com>
2023-01-30 19:42:57 +0500

    try/except

---
+++ try_except.py
@@ -0,0 +1,10 @@
+try:
+    with open('myfile.txt', 'w') as data:
+        print(input('Введите фразу: '), file=data)
+    with open('myfile.txt') as data:
+        data_file = data.read()
+    print(data_file)
+except NameError:
+    print('File not found.')
+except io.UnsupportedOperation:
+    print('File not writable')
