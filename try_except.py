6628bbac464b64bd72dc9b619ee7c4ea1875e810
Ivan <prigulnov88@gmail.com>
2023-01-30 19:57:35 +0500

    try/except

--- try_except.py
+++ try_except.py
@@ -4,7 +4,9 @@ try:
     with open('myfile.txt') as data:
         data_file = data.read()
     print(data_file)
-except NameError:
-    print('File not found.')
-except io.UnsupportedOperation:
-    print('File not writable')
+# except NameError:
+#     print('File not found.')
+# except io.UnsupportedOperation:
+#     print('File not writable')
+except Exception as err:
+    print(err)
