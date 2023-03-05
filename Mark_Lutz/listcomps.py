d9b3dc2191260d6f00a338ae1d167ba058efa442
Ivan <prigulnov88@gmail.com>
2023-03-05 19:06:14 +0500

    изучение listcomps

--- Mark_Lutz/listcomps.py
+++ Mark_Lutz/listcomps.py
@@ -1,16 +1,34 @@
 # import time
+from pprint import pprint
 
 # start = time.time()
 squares = [i * i for i in range(20)]
 
-even_squares = [y for i in range(1000) if (y := i * i) >= 100]
+even_squares = [y for i in range(20) if (y := i * i) % 2 == 0 and y > 0]
 # end = time.time()
 
-word = 'Что - Такое'
+text = 'ЧтО - Такое ЛеТо'
 
-new_word = ''.join(
-    [letter.lower() for letter in word]
-)
+new_text = ''.join([letter.lower() for letter in text])
+
+# translit = {'ч': 'ch', 'т': 't', 'а': 'a', 'к': 'k'}
+
+new_text2 = ' '.join(
+    [letter for letter in text.split() if letter != '-']
+).capitalize()
+
+ints = [-1, -2, -3, 7, 3, 2, 0]
+
+positives = [i for i in ints if i > 0]
+
+matrix = [list(range(i, i+3)) for i in range(3)]
+
 
 if __name__ == '__main__':
-    print(new_word)
+    print(squares)
+    print(even_squares)
+    print(new_text)
+    print(new_text2)
+    print(ints)
+    print(positives)
+    pprint(matrix, width=13)
